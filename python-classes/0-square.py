eb21b5a91b541c33f92ac8c378e899e3ee033bb5
ayxanveli <ayxanveli27@gmail.com>
2025-05-13 14:22:10 +0000

    task0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class Square:
+    """Defines a square"""
+    
     def __init__(self, size):
-        self.__size = size  # Private instance attribute
+        self.__size = size
