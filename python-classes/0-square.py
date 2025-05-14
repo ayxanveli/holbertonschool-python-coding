a14ab1a68218fc8b7e3ab159dfa4389129fba1f0
ayxanveli <ayxanveli27@gmail.com>
2025-05-14 12:20:59 +0000

    tddthfhtfh

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""Defines a class Square"""  # <--- Bunu əlavə etməsən testdən keçməyəcək
 
 class Square:
     """Represents a square"""
 
     def __init__(self, size):
         self.__size = size
+
 
