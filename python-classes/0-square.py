539e0dd77face24d8bda505be603fe581f35dd5a
ayxanveli <ayxanveli27@gmail.com>
2025-05-14 12:17:36 +0000

    ygfjgfjgsdjf

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
 class Square:
-    """Defines a square"""
-    
+    """Represents a square"""
+
     def __init__(self, size):
         self.__size = size
+
